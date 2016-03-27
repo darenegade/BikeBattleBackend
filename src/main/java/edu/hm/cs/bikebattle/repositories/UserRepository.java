b967ce3c6b066fc3c5605420cefe70e2dc40ed2e
René Zarwel <darenegade@gmx.de>
2016-03-27 20:30:54 +0200

    Initial commit

---
+++ src/main/java/edu/hm/cs/bikebattle/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package edu.hm.cs.bikebattle.repositories;
+
+import edu.hm.cs.bikebattle.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+/**
+ * Organization: HM FK07.
+ * Project: BikeBattleBackend, edu.hm.cs.bikebattle.repositories
+ * Author(s): Rene Zarwel
+ * Date: 27.03.16
+ * OS: MacOS 10.11
+ * Java-Version: 1.8
+ * System: 2,3 GHz Intel Core i7, 16 GB 1600 MHz DDR3
+ */
+@RepositoryRestResource(exported = true,
+        path="user", collectionResourceRel="users")
+public interface UserRepository extends CrudRepository<User, UUID> {
+}
